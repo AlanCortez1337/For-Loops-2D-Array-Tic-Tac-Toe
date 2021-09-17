183a845b5f4f182b69f9f9aa98d26484ef5a3fa0
Alan Cortez <78040578+AlanCortez1337@users.noreply.github.com>
2021-09-17 16:16:36 -0700

    Feature/problem files (#5)
    * Squashed 'problem/tools/cppaudit/' content from commit 3c99d41
    
    git-subtree-dir: problem/tools/cppaudit
    git-subtree-split: 3c99d4116298a2f9d4a13d2841faf0ebbc43d818
    
    * Need to review the unit tests hints
    
    * fixed hints and format from unittest
    
    * final check for all the changes and implimentations of unittest.cc
    
    * final check for all the changes and implimentations of unittest.cc
    
    * Finished game.cc game.h main.cc for problem folder
    
    * reworded the TODO the programmer needs to fulfill
    
    * reworded the TODO the programmer needs to fulfill
    
    * FINAL: reworded the TODO the programmer needs to fulfill
    
    * reworded the main.cc document, as well as game.h
    
    * reworded the main.cc document

---
+++ problem/game.cc
@@ -0,0 +1,85 @@
+#include "game.h"
+
+#include <iostream>
+
+bool PlayerWonCheck(std::vector<std::vector<std::string>> grid, int player) {
+  // TO DO:
+  // 1. Implement a way to identify horizontal, vertical and diagonal streaks
+  // using the two-dimensional vector "grid"
+  // 2. Print the current player and their winning streak on the console
+  // 3. Return true when a player has won any streak
+  return false;
+}
+/*
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+                              DO NOT MODIFY
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+*/
+void PlayerInput(std::vector<std::vector<std::string>> &grid,
+                 std::map<std::string, std::vector<int>> &moves,
+                 std::vector<std::string> &playerOptions, std::string input,
+                 int player) {
+  bool passed = false;
+  int x, y;
+  // We are using a do while because we want to run this at least once to
+  // check if the input valid and is a key in the map
+  do {
+    // This conditional is a fancy way in C++ to find if there is a key in a map
+    if (moves.find(input) != moves.end()) {
+      // We have to make this true in order to break out of the do-while loop
+      passed = true;
+      // These are to store the values from the map, which are coordinates on
+      // the grid. In other words, the values of the map is a vector where the
+      // value at [0] is x and [1] is y
+      x = moves[input][0];
+      y = moves[input][1];
+      // The following is to delete the key from the map and the
+      // array so that players cannot call the same action twice, causing
+      // overlap
+      moves.erase(input);
+      for (int optionIndex = 0; optionIndex < playerOptions.size();
+           optionIndex++) {
+        if (playerOptions[optionIndex] == input) {
+          playerOptions.erase(playerOptions.begin() + optionIndex);
+        }
+      }
+    } else {
+      std::cout << "Invalid input, try again: ";
+      std::cin >> input;
+    }
+  } while (!passed);
+  // This just overrides the "_" on the grid with the corresponding players mark
+  if (player == 1) {
+    grid[x][y] = "X";
+  } else {
+    grid[x][y] = "O";
+  }
+}
+
+void Display(std::vector<std::vector<std::string>> grid,
+             std::vector<std::string> possibleMoves, int canDisplay) {
+  for (int rows = 0; rows < grid.size(); rows++) {
+    // For loop to go through and print what value is at [rows][columns]
+    for (int columns = 0; columns < grid.size(); columns++) {
+      std::cout << grid[rows][columns];
+    }
+    // Necessary indentation once a row is complete to simulate a 3x3 grid
+    std::cout << std::endl;
+  }
+  // This if statement is meant as a simple way to not print the following once
+  // there are no more possible moves to play
+  if (canDisplay == 1) {
+    std::cout << "Possible input moves: ";
+    for (int moves = 0; moves < possibleMoves.size(); moves++) {
+      // The goal behind this if statement is to print "Possible moves: TL, TM,
+      // TR" where there is a comma inbetween each move and no comma after the
+      // last value
+      if (moves == possibleMoves.size() - 1) {
+        std::cout << possibleMoves[moves];
+      } else {
+        std::cout << possibleMoves[moves] << ",";
+      }
+    }
+    std::cout << "\n";
+  }
+}
--- problem/main.cc
+++ problem/main.cc
@@ -1,1 +1,75 @@
-## CPP audit suggests naming the driver main.cc
+#include <iostream>
+#include <map>
+#include <string>
+#include <vector>
+
+#include "game.h"
+/*
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+                              DO NOT MODIFY
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+*/
+int main() {
+  // Initialization:
+  // We are using a map because we want to attribute a move like "TL" or "Top
+  // Left" to a coordinate on the 2d grid, in this case: [0,0]
+  std::map<std::string, std::vector<int>> playerMoves = {
+      {"TL", {0, 0}}, {"TM", {0, 1}}, {"TR", {0, 2}},
+      {"ML", {1, 0}}, {"MM", {1, 1}}, {"MR", {1, 2}},
+      {"BL", {2, 0}}, {"BM", {2, 1}}, {"BR", {2, 2}}};
+  // This vector is mainly used as an easy way to print out the possible moves
+  std::vector<std::string> moveOptions = {"TL", "TM", "TR", "ML", "MM",
+                                          "MR", "BL", "BM", "BR"};
+  // The following is our 2d array which will be used to navigate through.
+  // Another way of looking at this is:
+  // {"_", "_", "_"}
+  // {"_", "_", "_"}
+  // {"_", "_", "_"}
+  // where the top left is [0,0] and the bottom right is [2,2]
+  std::vector<std::vector<std::string>> game_board = {
+      {"_", "_", "_"}, {"_", "_", "_"}, {"_", "_", "_"}};
+  // Other necessary variables we need to initialize to get the code working
+  int player = 1;
+  std::string input;
+  int x, y;
+  // This is just basic welcome dialog for when the code first runs
+  std::cout << "\n~~~~~~~~~~~~~~~~~~~~~~\n";
+  std::cout << "WELCOME TO TIC-TAC-TOE";
+  std::cout << "\n~~~~~~~~~~~~~~~~~~~~~~\n";
+  std::cout << "For inputs: The first letter is the row and the second is the "
+               "column.\n";
+  std::cout << "EX: \"TL\" is Top Left\nHere is the Grid: \n";
+  // This will be our main loop to keep on running until the game is over
+  while (true) {
+    // First time Display gets calls is to show the blank board and the possible
+    // moves. After the first time, calling this function will show an updated
+    // version of the game board and the possible moves the player can input
+    Display(game_board, moveOptions, 1);
+    std::cout << "Player " << player << " input: ";
+    std::cin >> input;
+    // Then this function will be where we use the new player's input to change
+    // how the grid will look in the backend
+    PlayerInput(game_board, playerMoves, moveOptions, input, player);
+    // Here we have a conditional to break out of the loop if any player has a
+    // winning streak so the loop won't run infinitely
+    if (PlayerWonCheck(game_board, player)) {
+      Display(game_board, moveOptions, 0);
+      break;
+      // Here is a conditional to exit the loop if there is a tie or if there
+      // are no more moves to play
+    } else if (moveOptions.size() == 0) {
+      Display(game_board, moveOptions, 0);
+      std::cout << "NO WINNERS: TIE" << std::endl;
+      break;
+    }
+    // These if statements are just to swap the player number once their turn
+    // is over.
+    if (player == 1) {
+      player = 2;
+    } else {
+      player = 1;
+    }
+  }
+
+  return 0;
+}
